94449e2d727997c31da37a95bd3367e0badcbcf3
Shreyam Singh <109955150+Shreyam-007@users.noreply.github.com>
2023-09-16 08:52:14 +0000

    comparison added

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+console.log(true > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(typeof(console.log(null == 0)));
+console.log(typeof(console.log(null > 0)));
+console.log(null >= 0);
+// comparison operator (>,<,>=,<=) and equality check operator (==) work differntly
+// in JS comparison operator (>,<,>=,<=) convert the null to 0, whereas equality check operator convert it into NaN.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// == check for value only, === check for value as well as datatype 
+
+console.log("4" == 2);
